13c3ce279269d7b21ba0de8d9c35cf6ff0096c69
havan <alexaleaxalex@gmail.com>
2021-07-23 17:35:40 -0300

    iniciando winForm

---
+++ Data/Repository/CategoriaRepository.cs
@@ -0,0 +1,27 @@
+﻿using Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Repository
+{
+    class CategoriaRepository
+    {
+        private List<Categoria> categoria;
+        public CategoriaRepository()
+        {
+            categoria = new List<Categoria>();
+        }
+        public void Create(Categoria model)
+        {
+            categoria.Add(model);
+        }
+        public List<Categoria> Listar()
+        {
+            return categoria;
+        }
+
+    }
+}
---
+++ Data/Repository/ProdutoResitory.cs
@@ -0,0 +1,26 @@
+﻿using Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Repository
+{
+    class ProdutoResitory
+    {
+        private List<Produto> produto;
+        public ProdutoResitory()
+        {
+            produto = new List<Produto>();
+        }
+        public void Create(Produto model)
+        {
+            produto.Add(model);
+        }
+        public List<Produto> Listar()
+        {
+            return produto;
+        }
+    }
+}
